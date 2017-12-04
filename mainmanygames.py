42c62cfdaa199c29164a3649c03a9a283c836683
panjidrama <panjidrama@gmail.com>
2017-12-04 12:28:10 +0100

    Update and rename main.py to mainmanygames.py

--- mainmanygames.py
+++ mainmanygames.py
@@ -1,10 +1,24 @@
-#Dicegame1
 """" Ce programme simule le tirage de trois dés à 6 faces aux valeurs non canoniques. Pour chaque dé à une valeur inférieure, le joueur gagne 1 point. Pour chaque dé à valeur supérieure, il perd un point. 
-Ceci simule des parties de 100 rounds.
-""""
 
-import random
-#Dés
+""""
+#Dicegame1
+print ('Games ?')
+nb_games = input()
+print ('Rounds/game ?')
+nb_round = input()
+
+    a_victorys = 0
+    b_victorys = 0
+    c_victorys = 0
+
+j = 0
+    while j > nb-games:
+
+
+
+
+    import random
+    #Dés
 DA = [2, 2, 4, 4, 9, 9]
 DB = [1, 1, 6, 6, 8, 8]
 DC = [3, 3, 5, 6, 7, 7]
@@ -16,7 +30,7 @@ c=0
 
 #Tirages
 i = 0
-while i < 100:
+while i < nb_rounds:
 
     TA = DA[random.randint(0, 5)]
     TB = DB[random.randint(0, 5)]
@@ -58,7 +72,20 @@ while i < 100:
 
     i =     i + 1
 
-print ('–––––––––––––––––Résultats finaux––––––––––––––––––––')
+""""print ('–––––––––––––––––Résultats finaux––––––––––––––––––––')
 print (a)
 print (b)
-print (c)
+print (c)""""
+
+    if a < b and a < c:
+        a_victorys = a + 1
+    if b>c and b>a:
+        b_victorys = b + 1
+    if c>b and c>a:
+        c_victorys = c + 1
+        
+        j = j + 1
+print ('_________________________')        
+print (a_victorys)
+print (b_victorys)
+print (c_victorys)
